b99ae7750bbe3b097be49e79791bed5d1a2dfb3a
Suvrodeb <suvrodevhowlader1408@gmail.com>
2025-01-24 19:12:58 +0600

    Making Route

---
+++ src/Pages/AboutUS/AboutUS.tsx
@@ -0,0 +1,9 @@
+const AboutUS = () => {
+  return (
+    <div>
+      <h1>About US</h1>
+    </div>
+  );
+};
+
+export default AboutUS;
--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -1,6 +1,11 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App";
 import Home from "../Pages/Home/Home";
+import AllProducts from "../Pages/AllProducts/AllProducts";
+import ProductDetail from "../Pages/ProductDetail/ProductDetail";
+import AboutUS from "../Pages/AboutUS/AboutUS";
+import Login from "../Pages/Login/Login";
+import Registration from "../Pages/Registration/Registration";
 
 export const router = createBrowserRouter([
   {
@@ -12,9 +17,29 @@ export const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/home",
+        path: "home",
         element: <Home />,
       },
+      {
+        path: "all-products",
+        element: <AllProducts />,
+      },
+      {
+        path: "product-detail",
+        element: <ProductDetail />,
+      },
+      {
+        path: "about-us",
+        element: <AboutUS />,
+      },
     ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "registration",
+    element: <Registration />,
   },
 ]);
