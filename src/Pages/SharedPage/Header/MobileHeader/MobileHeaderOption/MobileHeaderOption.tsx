d86887198a3f2e5b0f2e301024ec02f40d8feded
Suvrodeb <suvrodevhowlader1408@gmail.com>
2025-01-28 11:58:59 +0600

    Update mobile header

--- src/Pages/SharedPage/Header/MobileHeader/MobileHeaderOption/MobileHeaderOption.tsx
+++ src/Pages/SharedPage/Header/MobileHeader/MobileHeaderOption/MobileHeaderOption.tsx
@@ -1,6 +1,13 @@
-import { NavLink } from "react-router";
+import { useDispatch } from "react-redux";
+import { Link, NavLink } from "react-router";
+import { useAppSelector } from "../../../../../Redux/hooks";
+import DashboardCart from "../../DesktopHeader/DashboardCart";
+import DashboradButton from "../../DesktopHeader/DashboradButton";
+import { logout } from "../../../../../Redux/api/features/auth/authSlice";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const MobileHeaderOption = ({ handleClick }:any) => {
+const MobileHeaderOption = ({ handleClick }: any) => {
+  const { token } = useAppSelector((state) => state.auth);
+  const dispatch = useDispatch();
   return (
     <div className="bg-green-500 text-white flex flex-col gap-4 px-5 py-5 relative z-10">
       <div className="flex flex-col gap-4 font-bold w-full  ">
@@ -11,50 +18,41 @@ const MobileHeaderOption = ({ handleClick }:any) => {
         >
           Home
         </NavLink>
+
         <NavLink
           className={({ isActive }) => (isActive ? "text-blue-500" : "acLk ")}
-          to="/banner"
+          to="/all-books"
           onClick={() => handleClick(false)}
         >
-          Banner
-        </NavLink>
-        <NavLink
-          className={({ isActive }) => (isActive ? "text-blue-500" : "acLk ")}
-          to="/data"
-          onClick={() => handleClick(false)}
-        >
-          Data
+          All Book
         </NavLink>
 
         <NavLink
           className={({ isActive }) => (isActive ? "text-blue-500" : "acLk ")}
-          to="/imagecard"
+          to="/about-us"
           onClick={() => handleClick(false)}
         >
-          Image Card
+          About Us
         </NavLink>
 
-        <NavLink
-          className={({ isActive }) => (isActive ? "text-blue-500" : "lk")}
-          to="/hdr"
-        >
-          Header
-        </NavLink>
-
-        <NavLink
-          className={({ isActive }) => (isActive ? "text-blue-500" : "lk")}
-          to="/modal"
-          onClick={() => handleClick(false)}
-        >
-          Modal
-        </NavLink>
-        <NavLink
-          className={({ isActive }) => (isActive ? "text-blue-500" : "lk")}
-          to="/contact"
-          onClick={() => handleClick(false)}
-        >
-          Contact
-        </NavLink>
+        <div className="w-[25%]  flex justify-end items-center 0">
+          {token ? (
+            <div className=" flex items-center gap-x-3">
+              <DashboardCart />
+              <DashboradButton />
+              <button
+                className=" btn btn-warning text-white "
+                onClick={() => dispatch(logout())}
+              >
+                Logout
+              </button>
+            </div>
+          ) : (
+            <Link to={"/login"}>
+              <button className=" btn btn-primary text-white ">Login</button>
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
